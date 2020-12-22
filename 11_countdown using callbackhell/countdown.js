5b3d2d4943d41f4e9f15e0231d3fc2e3fe0f8511
csesumathi <61107406+csesumathi@users.noreply.github.com>
2020-12-22 16:31:17 +0530

    Add files via upload

---
+++ 11_countdown using callbackhell/countdown.js
@@ -0,0 +1,39 @@
+var time=document.getElementById('time')
+setTimeout(function () {
+    time.innerHTML = 10
+    setTimeout(function () {
+        time.innerHTML = 9
+        setTimeout(function () {
+            time.innerHTML = 8
+            setTimeout(function () {
+                time.innerHTML = 7
+                setTimeout(function () {
+                    time.innerHTML = 6
+                    setTimeout(function () {
+                        time.innerHTML = 5
+                        setTimeout(function () {
+                            time.innerHTML = 4
+                            setTimeout(function () {
+                                time.innerHTML = 3
+                                setTimeout(function () {
+                                    time.innerHTML = 2
+                                    setTimeout(function () {
+                                        time.innerHTML = 1
+                                        setTimeout(function () {
+                                            time.innerHTML = 0
+                                            setTimeout(function () {
+                                                time.innerHTML = "Happy Independence Day"
+                                                
+                                            })
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+},1000)
